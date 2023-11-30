c96d89d90488814e156fa534bc6f041cf49b6364
maryanahxavier <maryanahxavi9@gmail.com>
2023-11-30 16:30:51 -0300

    vamomerda

--- start/routes.ts
+++ start/routes.ts
@@ -20,14 +20,13 @@
 
 import Route from '@ioc:Adonis/Core/Route'
 
-
 Route.get('/', async () => {
   return { hello: 'world' }
 })
 
 Route.post('/register', 'AuthController.register')
 Route.post('/login', 'AuthController.login')
-Route.group (() => {
-  Route.put("/user" ,  "UsersController.update")
+Route.group(() => {
+  Route.put("/user", "UsersController.update")
+  Route.post("/push", "SendPushesController.send")
 }).middleware('auth')
-
